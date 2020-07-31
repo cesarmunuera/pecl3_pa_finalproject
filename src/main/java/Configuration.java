916e7cd807b512bb32cb7499fd35d9d070d79944
Cesar <cesar.munuera@gmail.com>
2020-07-31 12:55:22 +0200

    cambios 31/7 13:00

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -6,6 +6,7 @@ public class Configuration {
 	public static final int MAX_FLOOR = 20;
 	public static final int MAX_PEOPLE = 8;
 	public static final int NONE_FLOOR_CODE = -9876;
+        public static final int MAX_TIME_GENERATE_USER = 1500; //Miliseconds. Its between 0.5 & 2 seconds. So math.random + 0.5 .
 	public static final int MIN_BROKE_SECONDS = 20;
 	public static final int MAX_BROKE_SECONDS = 30;
 	public static final int MIN_REPAIR_SECONDS = 10;
