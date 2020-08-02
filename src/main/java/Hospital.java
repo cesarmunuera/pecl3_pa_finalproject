e3d22b931d561a86579e12a780a6cd532c2dfb22
ebustosm6 <eduardobustosmiranda@hotmail.com>
2020-08-02 23:00:40 +0200

    fill hospital

--- src/main/java/Hospital.java
+++ src/main/java/Hospital.java
@@ -1,5 +1,14 @@
-
+import java.util.ArrayList;
 
 public class Hospital {
+	
+	ArrayList<HospitalFloor> floors = new ArrayList<>();
+	
+	public Hospital() {
+		//TODO: check if floor 20 or until 19
+		for(int i=Configuration.MIN_FLOOR; i<Configuration.MAX_FLOOR; i++) { 
+			floors.add(new HospitalFloor(i));
+		}
+	}
 
 }
